906a9ce655aa33833bd77d696ba70cae7342f109
Guiselinii <159034639+Guiselinii@users.noreply.github.com>
2024-04-10 11:53:51 -0300

    Add files via upload

---
+++ Prova/pagina-inicial/bicicleta.js
@@ -0,0 +1,5 @@
+document.getElementById("botao-menu").addEventListener("click", function() {
+    var opcoesMenu = document.getElementById("opcoes-menu");
+    opcoesMenu.style.display = opcoesMenu.style.display === "block" ? "none" : "block";
+  });
+  
